7770bc2b72b09752e93a2dbca8df876b1d57221a
bhavesh357 <bkadam357@gmail.com>
2020-06-20 22:59:47 +0530

    ADDED: greetingServiceInterface

--- src/main/java/com/firstspringapp/controllers/GreetingController.java
+++ src/main/java/com/firstspringapp/controllers/GreetingController.java
@@ -2,6 +2,7 @@ package com.firstspringapp.controllers;
 
 import com.firstspringapp.model.Greeting;
 import com.firstspringapp.model.User;
+import com.firstspringapp.service.IGreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
