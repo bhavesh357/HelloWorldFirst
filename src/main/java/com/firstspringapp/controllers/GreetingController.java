09573c3d266a0699f0b91e36cb9ae3b6e487afec
bhavesh357 <bkadam357@gmail.com>
2020-06-20 22:57:06 +0530

    REFACTORED: controller to use service

--- src/main/java/com/firstspringapp/controllers/GreetingController.java
+++ src/main/java/com/firstspringapp/controllers/GreetingController.java
@@ -1,20 +1,26 @@
 package com.firstspringapp.controllers;
 
 import com.firstspringapp.model.Greeting;
+import com.firstspringapp.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@RequestMapping("/greeting")
 public class GreetingController {
-    private static final String template="Hello %s!";
-    private final AtomicLong counter= new AtomicLong();
 
-    @GetMapping("/greeting")
+    @Autowired
+    private IGreetingService greetingService;
+
+    @GetMapping("")
     public Greeting greeting(@RequestParam(value = "name",defaultValue = "World") String name){
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template,name));
+        User user = new User();
+        user.setFirstName(name);
+        return greetingService.addGreeting(user);
     }
 }
